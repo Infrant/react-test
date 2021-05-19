1e196a7cad208a43e4e9bf3839c7db35263c2480
Yury Glushko <yury.glushko.web@gmail.com>
2021-05-19 15:52:44 +0300

    fix redux_devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,12 +6,8 @@ const reducers = combineReducers({
   table: tableReducer,
 });
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
